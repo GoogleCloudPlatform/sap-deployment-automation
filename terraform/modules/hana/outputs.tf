9d274b91e88d5ec43086202388fb7461c85291d9
Bala Bharat Guduru <balaguduru@Jayamadhuris-MacBook-Pro.local>
2020-09-10 15:49:12 -0700

    Define required tf outputs in module

--- terraform/modules/hana/outputs.tf
+++ terraform/modules/hana/outputs.tf
@@ -30,10 +30,10 @@ output "public_ip" {
   value = google_compute_instance.gcp_sap_hana.network_interface[0].access_config[0].nat_ip
 }
 
-output "device_name_hana_data" {
-  value = google_compute_attached_disk.attached_disk_0.device_name
+output "instance_attached_disks_data" {
+  value = google_compute_disk.gcp_sap_hana_sd_data.name
 }
 
-output "device_name_hana_backup" {
-  value = google_compute_attached_disk.attached_disk_1.device_name
+output "instance_attached_disks_backup" {
+  value = google_compute_disk.gcp_sap_hana_sd_backup.name
 }
