7af40c75e2db0ca577183f113f34119a34002e30
Joseph Wright <joseph.wright@googlecloud.corp-partner.google.com>
2021-03-04 19:54:02 +0000

    Add LICENSE file and header to sources

--- terraform/modules/nw/outputs.tf
+++ terraform/modules/nw/outputs.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 output "instance_name" {
   description = "Name of Netweaver instance"
   value       = google_compute_instance.gcp_nw.name
--- terraform/modules/nw/variables.tf
+++ terraform/modules/nw/variables.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 variable "project_id" {
   description = "The ID of the project in which the resources will be deployed."
   default     = "albatross-duncanl-sandbox-2"
