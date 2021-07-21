2b8ec413125e25d1ceeb2ddf5bb3db3f295a281b
Maciej Zachariasz <mzachariasz@google.com>
2021-07-21 06:48:34 +0000

    New SAP instance type: Web Dispatcher. Installed on a separate VM.

---
+++ ansible/roles/wd-standard/README.md
@@ -0,0 +1,3 @@
+# nw-standard
+
+This is a high-level role which includes other roles, for the purpose of configuring a NetWeaver or S4HANA standard instance. This will normally be consumed by running the playbook `stacks/NetWeaver-Standard/playbook.yml`, which also handles running Terraform to create machines and set up HANA.
---
+++ stacks/WebDisp/tf/outputs.tf
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+output "inventory" {
+  value = {
+    "wd"   = [module.gcp_webdisp.instance_internal_ip],
+  }
+}
+
+output "wd_instance_name" {
+  description = "Name of Web Dispatcher instance."
+  value       = module.gcp_webdisp.instance_name
+}
+
+output "zone" {
+  description = "Compute Engine instance deployment zone"
+  value       = var.zone
+}
