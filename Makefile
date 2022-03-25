dfde143d53f3357ea395c9e160c71ba209efdac4
Marko Vlahovic <vlahovic@google.com>
2022-03-25 14:13:21 -0700

    Add linting capability
    This commit introduces linting capabilities for this repo. Linting
    is done via a container that defined in build/Dockerfile. Linting
    is implemented using the following techonologies:
    - ansilbe code linting via ansible-lint
    - terraform code linting via terraform fmt and terraform validate
    - python code linting via flake8
    - shell code linting via shellcheck
    In addition to the above we also check if the files have proper
    license header requires by Google PSO.
    This commit also introduces cloud-build configuration that is
    initial step towards CI/CD for this repo.
    Linting is NOT performed on the third_party/ and venv/ directories
    
    The code has been inspired by CFT CI/CD code. Please see
    https://github.com/GoogleCloudPlatform/cloud-foundation-toolkit/tree/master/infra/build/developer-tools
    for more information

---
+++ Makefile
@@ -0,0 +1,49 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Please note that this file was generated from [terraform-google-module-template](https://github.com/terraform-google-modules/terraform-google-module-template).
+# Please make sure to contribute relevant changes upstream!
+
+# Make will use bash instead of sh
+SHELL := /usr/bin/env bash
+
+DOCKER_IMAGE_DEVELOPER_TOOLS := sap-gcp-developer-tools
+DOCKER_TAG_VERSION_DEVELOPER_TOOLS := 0.1
+DOCKER_BIN ?= docker
+
+# Build docker container for local development
+.PHONY: docker_build
+docker_build:
+	$(DOCKER_BIN) build -t ${DOCKER_IMAGE_DEVELOPER_TOOLS}:${DOCKER_TAG_VERSION_DEVELOPER_TOOLS} build/
+
+# Enter docker container for local development
+.PHONY: docker_run
+docker_run:
+	$(DOCKER_BIN) run --rm -it \
+		-v "$(CURDIR)":/workspace \
+		${DOCKER_IMAGE_DEVELOPER_TOOLS}:${DOCKER_TAG_VERSION_DEVELOPER_TOOLS} \
+		/bin/bash
+
+# Run lint
+.PHONY: docker_lint
+docker_lint:
+	$(DOCKER_BIN) run --rm -it \
+		-v "$(CURDIR)":/workspace \
+		${DOCKER_IMAGE_DEVELOPER_TOOLS}:${DOCKER_TAG_VERSION_DEVELOPER_TOOLS} \
+		/usr/local/bin/test_lint.sh
+
+# Run cloud build locally. This is meant only for local testing of the cloud-build configuration
+.PHONY: cbl_lint
+cbl_lint:
+	cloud-build-local --config=build/lint.cloudbuild.yaml --dryrun=false  .
---
+++ build/scripts/test_lint.sh
@@ -0,0 +1,103 @@
+#! /bin/bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -u
+
+# Import helper functions from Docker container
+# shellcheck disable=SC1091
+source /usr/local/bin/task_helper_functions.sh
+
+# All tests should execute regardless of failures so we can report at the end.
+# Perform 'set +e' after sourcing task_helper_functions.sh in case someone has
+# 'set -e' inside a module-specific task_helper_functions.sh file.
+set +e
+
+# constants
+MARKDOWN=0
+MARKDOWN_STR=""
+CONTRIBUTING_GUIDE=""
+# shellcheck disable=SC2089,SC2016  # Quotes/backslashes will be treated literally, expressions don't expand
+messages='{
+  "check_headers": "All files need a license header. Please make sure all your files include the appropriate header. A helper tool available [here](https://github.com/google/addlicense).",
+  "check_shell": "Failed shell check. More info on running shellcheck locally [here](https://www.shellcheck.net).",
+  "check_python": "Failed flake8 Python lint check.",
+  "check_terraform": "Failed Terraform check. More details below."
+  "check_ansible": "Failed ansible-lint check. More details below."
+  "check_whitespace": "Failed whitespace check. More details below.",
+}'
+rval=0
+failed_tests=()
+tests=(
+  check_shell
+  check_headers
+  check_python
+  check_terraform
+  check_ansible
+  # disabling check_whitespace for now. It produces too many errors at the moment.
+  #check_whitespace
+)
+
+# parse args
+for arg in "$@"
+do
+  case $arg in
+    -m|--markdown)
+      MARKDOWN=1
+      shift
+      ;;
+    -c=*|--contrib-guide=*)
+      CONTRIBUTING_GUIDE="${arg#*=}"
+      shift
+      ;;
+      *) # end argument parsing
+      shift
+      ;;
+  esac
+done
+
+for test in "${tests[@]}"; do
+  # if not in markdown mode, pipe test output to stdout tty
+  # nested if condition is a workaround for test[[]] not echoing some outputs from check_* tests even with subshell
+  if [[ $MARKDOWN -eq 0 ]]; then
+    if ! "${test}"; then
+      failed_tests+=("${test}")
+      ((rval++))
+    fi
+  # if control reaches here - in markdown mode, pipe test stderr to stdout for capture
+  elif ! output=$(${test} 2>&1); then
+    # add test name to list of failed_tests
+    failed_tests+=("${test}")
+    ((rval++))
+    # clean output color, sqash multiple empty blank lines
+    output=$(echo "$output" | sed -r "s/\x1b\[[0-9;]*m/\n/g" | tr -s '\n')
+    # try to get a helpful error message, otherwise unknown
+    error_help_message=$(echo "$messages" | jq  --arg check_name "$test" -r '.[$check_name] // "🦖 An unknown error has occurred" ')
+    #construct markdown body
+    MARKDOWN_STR+="- ⚠️${test}\n ${error_help_message} \n \`\`\`bash \n${output}\n \`\`\` \n"
+  fi
+done
+
+# if any tests have failed
+if [[ "${#failed_tests[@]}" -ne 0 ]]; then
+  # echo output in markdown
+  if [[ $MARKDOWN -eq 1 ]]; then
+  header="Thanks for the PR! 🚀\nUnfortunately it looks like some of our CI checks failed. See the [Contributing Guide](${CONTRIBUTING_GUIDE}) for details.\n"
+  echo -e "${header}${MARKDOWN_STR}"
+  else
+    # shellcheck disable=SC2145  # Output all elements of the array
+    echo "Error: The following tests have failed: ${failed_tests[@]}"
+    exit "${rval}"
+  fi
+fi
